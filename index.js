8b5f44f1945c1af590b128ebd5ed2ec84bf4c125
AngelRionCervi <a.rioncervi@gmail.com>
2019-12-28 02:30:46 +0100

    es6 class approach

--- index.js
+++ index.js
@@ -1,256 +1,264 @@
-let fullFrequencies;
-let amplitude;
-let cfg = {};
-let canvas;
-let radInc = 0;
-let radIncVal = 0;
-
-function nearestPow2(nbr) {
-    if (nbr <= 32) {
-        return 32;
-    } else {
-        return Math.pow(2, Math.round(Math.log(nbr) / Math.log(2))); 
+class CircularType {
+    constructor(canvas, audioSrc, cfg) {
+        Object.assign(this, cfg);
+        this.fullFrequencies;
+        this.amplitude;
+        this.canvas = canvas;
+        this.radInc = 0;
+        this.radIncVal = 0;
+        this.context;
+        this.analyser;
+        this.source;
+        this.audio;
+        this.audioSrc = audioSrc;
     }
-}
 
-function getAvg(arr) {
-    const total = arr.reduce((acc, c) => acc + c, 0);
-    return (total / arr.length) * cfg.circularType.innerCircleMult;
-}
-
-function splitUp(arr, n) {
-    let rest = arr.length % n // how much to divide
-    let restUsed = rest // to keep track of the division over the elements
-    let partLength = Math.floor(arr.length / n)
-    let result = [];
-
-    for (let i = 0; i < arr.length; i += partLength) {
-        let end = partLength + i
-        let add = false;
-
-        if (rest !== 0 && restUsed) { // should add one element for the division
-            end++;
-            restUsed--; // we've used one division element now
-            add = true;
-        }
-
-        let middle = Math.floor(end-(partLength/2))-1;
-        result.push(arr[middle]); // part of the array
-
-        if (add) {
-            i++; // also increment i in the case we added an extra element for division
+    nearestPow2(nbr) {
+        if (nbr <= 32) {
+            return 32;
+        } else {
+            return Math.pow(2, Math.round(Math.log(nbr) / Math.log(2)));
         }
     }
 
-    return result;
-}
-
-function initVisualizer(c, s, f) {
-    cfg = f;
-    canvas = c;
-    audio = new Audio();
-    context = new window.AudioContext();
-    analyser = context.createAnalyser();
-    audio.src = s; // the source path
-    source = context.createMediaElementSource(audio);
-    source.connect(analyser);
-    analyser.connect(context.destination);
-    analyser.fftSize = 2048;
-  
-    //frequencies = new Uint8Array(analyser.frequencyBinCount);
-    fullFrequencies = new Uint8Array(analyser.fftSize)
-    
-    //console.log(frequenciesFull, frequencies)
-    audio.play();
-
-    switch (cfg.type) {
-        case 'circular':
-            renderCircular();
-            break;
-    }
-}
-
-function renderCircular() {
-    
-    canvas.width = cfg.width;
-    canvas.height = cfg.height;
-    let ctx = canvas.getContext("2d");
-    
-    let centerX = canvas.width / 2;
-    let centerY = canvas.height / 2;
-
-    let freqs;
-    freqs = splitUp(fullFrequencies.slice(cfg.freqRange[0], cfg.freqRange[1]), cfg.barNbr)
-
-    if (cfg.circularType.skipNull) {
-        freqs = new Uint8Array(freqs.filter(el => el != 0));
-    } else {
-        freqs = new Uint8Array(freqs);
+    getAvg(arr) {
+        const total = arr.reduce((acc, c) => acc + c, 0);
+        return (total / arr.length) * this.circularType.innerCircleMult;
     }
 
-    let radius = cfg.circularType.innerCircleReact ? cfg.circularType.radius + getAvg(freqs) : cfg.circularType.radius;
-    radius += radInc;
-    ctx.fillStyle = cfg.backgroundColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-   
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
-    ctx.stroke();
+    splitUp(arr, n) {
+        let rest = arr.length % n // how much to divide
+        let restUsed = rest // to keep track of the division over the elements
+        let partLength = Math.floor(arr.length / n)
+        let result = [];
 
-    analyser.getByteFrequencyData(fullFrequencies);
+        for (let i = 0; i < arr.length; i += partLength) {
+            let end = partLength + i
+            let add = false;
 
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    
-    for (let i = 0; i < freqs.length; i++) {
-        
-        let barHeight = freqs[i] * cfg.barHeightMult + cfg.minBarHeight;
-        if (cfg.maxBarHeight && barHeight > cfg.maxBarHeight) {
-            barHeight = cfg.maxBarHeight;
+            if (rest !== 0 && restUsed) { // should add one element for the division
+                end++;
+                restUsed--; // we've used one division element now
+                add = true;
+            }
+
+            let middle = Math.floor(end - (partLength / 2)) - 1;
+            result.push(arr[middle]); // part of the array
+
+            if (add) {
+                i++; // also increment i in the case we added an extra element for division
+            }
         }
 
-        ctx.save();
-        ctx.rotate(i * Math.PI / (freqs.length*0.5));
-        ctx.fillStyle = getBarColor(freqs[i], ctx, barHeight);
-        joined(ctx, radius, -cfg.barWidth / 2, barHeight, cfg.barWidth, cfg.barBorderRadius, barHeight)
-   
-        ctx.restore();
+        return result;
     }
-    radInc += radIncVal;
-    requestAnimationFrame(renderCircular);
-}
 
+    init() {
 
-function getBarColor(frequency, ctx, barHeight) {
+        this.audio = new Audio();
+        this.context = new window.AudioContext();
+        this.analyser = this.context.createAnalyser();
+        this.audio.src = this.audioSrc;
+        this.source = this.context.createMediaElementSource(this.audio);
+        this.source.connect(this.analyser);
+        this.analyser.connect(this.context.destination);
+        this.analyser.fftSize = 2048;
 
-    let color;
+        //frequencies = new Uint8Array(analyser.frequencyBinCount);
+        this.fullFrequencies = new Uint8Array(this.analyser.fftSize)
 
-    let gradientRadius = cfg.gradientRadius;
-    let gradColors = cfg.gradientColors
-    let mult = cfg.colorFrequencyMultiplier;
+        //console.log(frequenciesFull, frequencies)
+        this.audio.play();
 
-    switch (cfg.barColorType) {
-        case "frequency":
-            if (cfg.mainColorChannel === "blue") {
-                color = "rgb(" + frequency * mult + ", " + frequency * mult + ", " + cfg.mainColorChannelValue + ")";
+        switch (this.type) {
+            case 'circular':
+                this.renderCircular();
+                break;
+        }
+    }
+
+    renderCircular() {
+
+        this.canvas.width = 500;
+        this.canvas.height = 500;
+
+        let ctx = this.canvas.getContext("2d");
+
+        let centerX = this.canvas.width / 2;
+        let centerY = this.canvas.height / 2;
+
+        let freqs = this.splitUp(this.fullFrequencies.slice(this.freqRange[0], this.freqRange[1]), this.barNbr)
+
+        if (this.circularType.skipNull) {
+            freqs = new Uint8Array(freqs.filter(el => el != 0));
+        } else {
+            freqs = new Uint8Array(freqs);
+        }
+
+        let radius = this.circularType.innerCircleReact ? this.circularType.radius + this.getAvg(freqs) : this.circularType.radius;
+        radius += this.radInc;
+        ctx.fillStyle = this.backgroundColor;
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+
+        this.analyser.getByteFrequencyData(this.fullFrequencies);
+
+        ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+
+        for (let i = 0; i < freqs.length; i++) {
+
+            let barHeight = freqs[i] * this.barHeightMult + this.minBarHeight;
+            if (this.maxBarHeight && barHeight > this.maxBarHeight) {
+                barHeight = this.maxBarHeight;
             }
-            if (cfg.mainColorChannel === "green") {
-                color = "rgb(" + frequency * mult + ", " + cfg.mainColorChannelValue + ", " + frequency * mult + ")";
-            }
-            if (cfg.mainColorChannel === "red") {
-                color = "rgb(" + cfg.mainColorChannelValue + ", " + frequency * mult + ", " + frequency * mult + ")";
-            }
-            break;
-        case "gradient":
-            let radius = gradientRadius === "barSize" ? barHeight : gradientRadius;
-            let gradient = ctx.createLinearGradient(0, 0, radius*cfg.gradientRadiusMultiplier, 0);
-            for (let u = 0; u < gradColors.length; u++) {
-                gradient.addColorStop((u/gradColors.length), gradColors[u]);
-            }  
-            color = gradient;
-            break;
-        case "fixedColor":
-            color = cfg.mainBarColor;
-            break;
-    }
-    return color;
-}
 
+            ctx.save();
+            ctx.rotate(i * Math.PI / (freqs.length * 0.5));
+            ctx.fillStyle = this.getBarColor(freqs[i], ctx, barHeight);
+            this.joined(ctx, radius, -this.barWidth / 2, barHeight, this.barWidth, this.barBorderRadius, barHeight, freqs[i])
 
-function roundRect(ctx, x, y, width, height, radius, barHeight) {
-    
-    if (radius <= barHeight) {
-        radius = {tl: radius, tr: radius, br: radius, bl: radius};
-    } else {
-        radius = {tl: 0, tr: 0, br: 0, bl: 0};
+            ctx.restore();
+        }
+        this.radInc += this.radIncVal;
+        requestAnimationFrame(() => this.renderCircular());
     }
 
-    if (!cfg.doubleBorderRadius) {
-        radius.bl = 0;
-        radius.tl = 0;
+
+    getBarColor(frequency, ctx, barHeight) {
+
+        let color;
+
+        let gradientRadius = this.gradientRadius;
+        let gradColors = this.gradientColors
+        let mult = this.colorFrequencyMultiplier;
+
+        switch (this.barColorType) {
+            case "frequency":
+                if (this.mainColorChannel === "blue") {
+                    color = "rgb(" + frequency * mult + ", " + frequency * mult + ", " + this.mainColorChannelValue + ")";
+                }
+                if (this.mainColorChannel === "green") {
+                    color = "rgb(" + frequency * mult + ", " + this.mainColorChannelValue + ", " + frequency * mult + ")";
+                }
+                if (this.mainColorChannel === "red") {
+                    color = "rgb(" + this.mainColorChannelValue + ", " + frequency * mult + ", " + frequency * mult + ")";
+                }
+                break;
+            case "gradient":
+                let radius = gradientRadius === "barSize" ? barHeight : gradientRadius;
+                let gradient = ctx.createLinearGradient(0, 0, radius * this.gradientRadiusMultiplier, 0);
+                for (let u = 0; u < gradColors.length; u++) {
+                    gradient.addColorStop((u / gradColors.length), gradColors[u]);
+                }
+                color = gradient;
+                break;
+            case "fixedColor":
+                color = this.mainBarColor;
+                break;
+        }
+        return color;
     }
 
-    ctx.beginPath();
-    ctx.moveTo(x + radius.tl, y);
-    ctx.lineTo(x + width - radius.tr, y);
-    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
-    ctx.lineTo(x + width, y + height - radius.br);
-    ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
-    ctx.lineTo(x + radius.bl, y + height);
-    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
-    ctx.lineTo(x, y + radius.tl);
-    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
-    ctx.closePath();
-    
-    ctx.fill();
-}
 
-function triangle(ctx, x, y, width, height, radius, barHeight) {
+    roundRect(ctx, x, y, width, height, radius, barHeight) {
 
-    if (radius <= barHeight) {
-        radius = {tl: radius, tr: radius, br: radius, bl: radius};
-    } else {
-        radius = {tl: 0, tr: 0, br: 0, bl: 0};
+        if (radius <= barHeight) {
+            radius = { tl: radius, tr: radius, br: radius, bl: radius };
+        } else {
+            radius = { tl: 0, tr: 0, br: 0, bl: 0 };
+        }
+
+        if (!this.doubleBorderRadius) {
+            radius.bl = 0;
+            radius.tl = 0;
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(x + radius.tl, y);
+        ctx.lineTo(x + width - radius.tr, y);
+        ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+        ctx.lineTo(x + width, y + height - radius.br);
+        ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+        ctx.lineTo(x + radius.bl, y + height);
+        ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+        ctx.lineTo(x, y + radius.tl);
+        ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+        ctx.closePath();
+
+        ctx.fill();
     }
 
-    if (!cfg.doubleBorderRadius) {
-        radius.bl = 0;
-        radius.tl = 0;
+    triangle(ctx, x, y, width, height, radius, barHeight) {
+
+        if (radius <= barHeight) {
+            radius = { tl: radius, tr: radius, br: radius, bl: radius };
+        } else {
+            radius = { tl: 0, tr: 0, br: 0, bl: 0 };
+        }
+
+        if (!this.doubleBorderRadius) {
+            radius.bl = 0;
+            radius.tl = 0;
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        /*
+        ctx.lineTo(x, y + width);
+        ctx.lineTo(y, x - height);
+        ctx.strokeStyle = "red"
+        ctx.lineWidth = 1*/ //sick flower
+
+        ctx.lineTo(x + width, y - 5);
+        ctx.lineTo(x + width, y + 5);
+
+        ctx.closePath();
+
+        ctx.fill();
     }
 
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    /*
-    ctx.lineTo(x, y + width);
-    ctx.lineTo(y, x - height);
-    ctx.strokeStyle = "red"
-    ctx.lineWidth = 1*/ //sick flower
+    joined(ctx, x, y, width, height, radius, barHeight, prevFreq, freq) {
 
-    ctx.lineTo(x + width, y-5);
-    ctx.lineTo(x + width, y+5);
-    
-    ctx.closePath();
-    
-    ctx.fill();
-}
+        if (radius <= barHeight) {
+            radius = { tl: radius, tr: radius, br: radius, bl: radius };
+        } else {
+            radius = { tl: 0, tr: 0, br: 0, bl: 0 };
+        }
 
-function joined(ctx, x, y, width, height, radius, barHeight, prevFreq, freq) {
+        if (!this.doubleBorderRadius) {
+            radius.bl = 0;
+            radius.tl = 0;
+        }
 
-    if (radius <= barHeight) {
-        radius = {tl: radius, tr: radius, br: radius, bl: radius};
-    } else {
-        radius = {tl: 0, tr: 0, br: 0, bl: 0};
+        ctx.beginPath();
+        ctx.moveTo(x + width, y);
+        /*
+        ctx.lineTo(x, y + width);
+        ctx.lineTo(y, x - height);*/ //sick flower
+
+        /* ctx.lineTo(x-width , y-5);
+        ctx.lineTo(x , y+5); */ //sharp rose
+        ctx.strokeStyle = "red"
+        ctx.lineWidth = 1
+        /*
+        ctx.lineTo(x, y);
+        ctx.lineTo(y, y-1);*/ //bar with middle circle
+        //ctx.lineTo(x+height , y);
+        /*
+        ctx.lineTo(x, y);
+        ctx.lineTo(y, x+width);*/ //sick geometry
+
+        /*ctx.lineTo(x, y);
+        ctx.lineTo(y+width, y);*/ //vertical lines
+        ctx.lineTo(x, y);
+        ctx.lineTo(y + width, x);
+        //ctx.closePath();
+
+        ctx.stroke();
     }
-
-    if (!cfg.doubleBorderRadius) {
-        radius.bl = 0;
-        radius.tl = 0;
-    }
-
-    ctx.beginPath();
-    ctx.moveTo(x + width, y);
-    /*
-    ctx.lineTo(x, y + width);
-    ctx.lineTo(y, x - height);*/ //sick flower
-
-    /* ctx.lineTo(x-width , y-5);
-    ctx.lineTo(x , y+5); */ //sharp rose
-    ctx.strokeStyle = "red"
-    ctx.lineWidth = 1
-    /*
-    ctx.lineTo(x, y);
-    ctx.lineTo(y, y-1);*/ //bar with middle circle
-    //ctx.lineTo(x+height , y);
-    /*
-    ctx.lineTo(x, y);
-    ctx.lineTo(y, x+width);*/ //sick geometry
-
-    /*ctx.lineTo(x, y);
-    ctx.lineTo(y+width, y);*/ //vertical lines
-    ctx.lineTo(x, y);
-    ctx.lineTo(y+width, y);
-    //ctx.closePath();
-    
-    ctx.stroke();
 }
 
 
